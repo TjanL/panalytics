e31c3f5cafe5f63da01331147ec121b95c86e222
Tjan <tljubesek@gmail.com>
2022-11-14 17:37:41 +0100

    Update convert.py

--- .github/scripts/convert.py
+++ .github/scripts/convert.py
@@ -34,7 +34,7 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    for path in Path(args.dir).glob("*.json"):
+    for path in Path(args.d).glob("*.json"):
         data = parse_file(path)
 
         with open(f"{Path(args.dir).joinpath(path.stem)}.csv", "w", newline="") as file:
